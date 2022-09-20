e719655ea4006303bceba457bfdb22f195ad0962
Dexvamalon <erik@hausner.se>
2022-09-20 13:46:55 +0200

    Continued on code

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,6 +8,30 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private List<Transform> obstacles = new List<Transform>();
     [SerializeField] private float levelSpeed = 1f;
 
+    [Header("Pathway variables")]
+    private bool[] activePathwayUp = new bool[4];
+    private bool[] activePathwayDown = new bool[4];
+
+    private bool onBrake = false;
+    private bool hasPlacedDown = false;
+
+    [SerializeField] private float pathLengthMax = 1f;
+    [SerializeField] private float pathLengthMin = 1f;
+
+    [SerializeField] private float pauseLengthMax = 1f;
+    [SerializeField] private float pauseLengthMin = 1f;
+
+    private float pathLength = 1f;
+
+    [SerializeField] private float placeChance = 1f; //(chance = 1/placeChance)
+
+    [SerializeField] private bool[] pathwayStructure1 = new bool[4]; // 2 high
+    [SerializeField] private bool[] pathwayStructure2 = new bool[4]; // single up
+    [SerializeField] private bool[] pathwayStructure3 = new bool[4]; // single down
+    [SerializeField] private bool[] pathwayStructure4 = new bool[4]; // double up
+    [SerializeField] private bool[] pathwayStructure5 = new bool[4]; // double down
+                                                                     //(bool array of 4 units 2 upp 2 down)
+
     void Start()
     {
         
@@ -24,32 +48,7 @@ public class LevelManager : MonoBehaviour
     void PlacePathway()
     {
         /*
-
-        [Header("Pathway variables")]
-        private bool[] activePathwayUp = new bool[4];
-        private bool[] activePathwayDown = new bool[4];
-
-        private bool onBrake = false;
-        private bool hasPlacedDown = false;
-
-        [Serializefield] private float pathLengthMax = 1f;
-        [Serializefield] private float pathLengthMin = 1f;
-
-        [Serializefield] private float pauseLengthMax = 1f;
-        [Serializefield] private float pauseLengthMin = 1f;
-
-        private float pathLength = 1f;
-
-        [Serializefield] private float placeChance = 1f; //(chance = 1/placeChance)
-
-        [Serializefield] private bool[] pathwayStructure1 = new bool[4]; // 2 high
-        [Serializefield] private bool[] pathwayStructure2 = new bool[4]; // single up
-        [Serializefield] private bool[] pathwayStructure3 = new bool[4]; // single down
-        [Serializefield] private bool[] pathwayStructure4 = new bool[4]; // double up
-        [Serializefield] private bool[] pathwayStructure5 = new bool[4]; // double down
-        //(bool array of 4 units 2 upp 2 down)
-
-
+        
         if(!onBrake)
         {
             hasPlacedDown = false;
@@ -58,38 +57,54 @@ public class LevelManager : MonoBehaviour
 
             for(int i = 0; i < pahts.count; i++)
             {
-                if(/random.range 1 placeChance == 1)
+                if(Random.Range(1, placeChance) == 1)
                 {
-                    /Switch random.range 1 5
-                    case1
-                    PlacePath(pathwayStructure1, i);
-                    case2
-                    PlacePath(pathwayStructure2, i);
-                    case3
-                    PlacePath(pathwayStructure3, i);
-                    case4
-                    PlacePath(pathwayStructure4, i);
-                    case5
-                    PlacePath(pathwayStructure5, i);
+                    Switch (Random.Range(1, 5))
+                    {
+                        case 1:
+                            PlacePath(pathwayStructure1, i);
+                            break;
+                        case 2:
+                            PlacePath(pathwayStructure2, i);
+                            break;
+                        case 3:
+                            PlacePath(pathwayStructure3, i);
+                            break;
+                        case 4:
+                            PlacePath(pathwayStructure4, i);
+                            break;
+                        case 5:
+                            PlacePath(pathwayStructure5, i);
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
-            if(!has placed down)
+            if(!hasPlacedDown)
             {
-                int row = random.range 0 3;
+                int row = Random.Range(0, 3);
 
-                /Switch random.range 1 3
-                case1
-                PlacePath(pathwayStructure1, row);
-                case2
-                PlacePath(pathwayStructure3, row);
-                case3
-                PlacePath(pathwayStructure5, row);
+                Switch (Random.Range(1, 3))
+                {
+                    case 1:
+                        PlacePath(pathwayStructure1, row);
+                        break;
+                    case 2:
+                        PlacePath(pathwayStructure3, row);
+                        break;
+                    case 3:
+                        PlacePath(pathwayStructure5, row);
+                        break;
+                    default:
+                        break;
+                }
             }
             onBrake = true;
         }
         else
         {
-            pathLength = /random.range pauseLengthMax pauseLengthMin
+            pathLength = Random.Range(pauseLengthMax, pauseLengthMin);
 
             for(int i = 0; i < path.count; i++)
             {
@@ -98,25 +113,25 @@ public class LevelManager : MonoBehaviour
 
             onBrake = false;
         }
-
          */
     }
 
     void PlacePath(bool[] pathwayStructure, int row)
     {
         /* 
-        if(0)
+        //Todo Maby make this a switch statement.
+        if(pathwayStructure[0])
         {
-           upp 0+(row) = true
+           activePathwayUp[row] = true;
         }
-        if(2)
+        if(pathwayStructure[2])
         {
-           down 0+(row) = true
-           hasPlacedDown = true
+           activePathwayDown[row] = true;
+           hasPlacedDown = true;
         }
-        if(1)
+        if(pathwayStructure[1])
         {
-           switch row
+           switch row  //Todo all here
            case1
            upp 1 = true
            case2
@@ -140,9 +155,10 @@ public class LevelManager : MonoBehaviour
            case4
            upp 2 = true
         }
-        if(3)
+
+        if(pathwayStructure[3])
         {
-           switch row
+           switch row     //Todo all here
            case1
            down 1 = true
            case2
