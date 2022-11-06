a76c04babc764af5c374a3154ab0a7ee654a1ff8
Dexvamalon <erik@hausner.se>
2022-11-06 18:23:33 +0100

    wow

---
+++ Assets/Scripts/ButtonCollison.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCollison : MonoBehaviour
+{
+    void Start()
+    {
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+    }
+
+    public void Scale()
+    {
+        transform.localScale = new Vector3(1.2f, 1.2f, 1);
+    }
+}
